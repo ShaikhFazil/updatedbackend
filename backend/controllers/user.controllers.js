c1879e51fb8507284915d7a99cd9a76b52478285
fazil <fazilshaikh@thehosteller.com>
2025-06-01 19:16:31 +0530

    first commit

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -135,7 +135,8 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         maxAge: 24 * 60 * 60 * 1000, // 1 day
         httpOnly: true,
-        sameSite: "strict",
+     secure: process.env.NODE_ENV === "production",
+    sameSite: "None",
       })
       .json({
         message: `Welcome Back ${user.fullname}`,
